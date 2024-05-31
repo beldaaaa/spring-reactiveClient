8e5b3a6c0ad8d3191788b43f52bcfb5f2108f710
beldaaaa <oldbanana@email.cz>
2024-05-31 20:36:40 +0200

    initial commit

---
+++ src/main/java/springframework/springreactiveclient/SpringReactiveClientApplication.java
@@ -0,0 +1,13 @@
+package springframework.springreactiveclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveClientApplication.class, args);
+    }
+
+}
